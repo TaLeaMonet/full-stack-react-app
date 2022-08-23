93e66b0bcc961c1986b9005e414fa741589e559b
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-08-23 10:49:40 -0700

    Fix styling. Test calling API.

--- full_stack_app_with_react_and_a_rest_api-v1/client/src/App.js
+++ full_stack_app_with_react_and_a_rest_api-v1/client/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Courses from './components/Courses';
 
 
-function App() {
+const  App = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/api/courses')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setCourses(data);
+    })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  }, []);
   return (
     <div>
    <Header />
+   <div className="course-container">
+      {courses.map((course) => {
+         return (
+            <div className="course-card" key={course.id}>
+               <h2 className="course-title">{course.title}</h2>
+               <p className="course-desc">{course.description}</p>
+               <p className="course-est-time">{course.estimatedTime}</p>
+               <p className="course-materials">{course.materialsNeeded}</p>
+            </div>
+         );
+      })}
    </div>
+   </div>
+   
   );
 }
 
